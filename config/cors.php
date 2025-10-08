f03375d22a620c90a5ebbd246152ed297af4bd54
Pai Zay Oo <paizay.o65@rsu.ac.th>
2025-10-08 10:34:03 +0700

    CORS

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,14 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+    ],
 
     'allowed_origins_patterns' => [],
 
