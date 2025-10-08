f7d44d728e853b3699f1b02f12e0ff8fbc78f04b
Pai Zay Oo <paizay.o65@rsu.ac.th>
2025-10-08 16:00:00 +0700

    CORS 3

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:3000',
+        'https://livenix.htetaungthant.com',
         'http://127.0.0.1:3000',
     ],
 
